19f02f26368c3dc2e2d0b59194007e2400406a47
JohanniH <60876904+JohanniH@users.noreply.github.com>
2023-10-13 21:40:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,80 @@
-// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the book list and book details elements
-    const bookList = document.getElementById("book-list-ul");
-    const bookDetails = document.getElementById("book-details");
-  
-    // Loop through the 'books' array and create HTML elements for each book
-    books.forEach(function (book, index) {
-      // Create a new list item for each book
-      const listItem = document.createElement("li");
-      listItem.textContent = `${book.title} by ${book.author} (${book.year})`;
-      
-      // Add a click event listener to display book details
-      listItem.addEventListener("click", function () {
-        displayBookDetails(index);
-      });
-  
-      // Append the list item to the book list
-      bookList.appendChild(listItem);
+  const bookList = document.getElementById("book-list-ul");
+  const bookDetails = {
+    title: document.getElementById("book-title"),
+    author: document.getElementById("book-author"),
+    year: document.getElementById("book-year"),
+    genre: document.getElementById("book-genre"),
+    rating: document.getElementById("book-rating"),
+    description: document.getElementById("book-description"),
+    image: document.getElementById("book-image"),
+  };
+
+  // Function to create and display book cards
+  function displayBookCards(books) {
+    bookList.innerHTML = "";
+    books.forEach((book, index) => {
+      const card = createBookCard(book, index);
+      bookList.appendChild(card);
     });
-  
-    // Function to display book details
-    function displayBookDetails(index) {
-      const selectedBook = books[index];
-  
-      // Update the content of the book details element
-      bookDetails.innerHTML = `
-        <h2>${selectedBook.title}</h2>
-        <p>ID: ${index + 1}</p>
-        <p>Author: ${selectedBook.author}</p>
-        <p>Publication Year: ${selectedBook.year}</p>
-        <p>Genre: ${selectedBook.genre}</p>
-        <p>Rating: ${selectedBook.rating}</p>
-        <p>Description: ${selectedBook.description}</p>
-        <img src="${selectedBook.image}" alt="${selectedBook.title}">
-      `;
-    }
+  }
 
-    // Get the genre filter dropdown element
-    const genreFilter = document.getElementById("genre-filter");
+// Function to create a book card
+function createBookCard(book, index) {
+  const card = document.createElement("div");
+  card.className = "card";
 
-    // Add an event listener to the genre filter dropdown
-    genreFilter.addEventListener("change", function () {
-        // Get the selected genre
-        const selectedGenre = genreFilter.value;
+  const image = document.createElement("img");
+  image.src = book.image;
+  image.alt = book.title;
 
-        // Filter the books based on the selected genre
-        const filteredBooks = selectedGenre === "all" ? books : books.filter(book => book.genre === selectedGenre);
+  const title = document.createElement("h3");
+  title.textContent = book.title;
 
-        // Clear the existing book list
-        bookList.innerHTML = '';
+  const author = document.createElement("p");
+  author.textContent = `Author: ${book.author}`;
 
-        // Loop through the filtered books and create list items
-        filteredBooks.forEach(function (book, index) {
-          const listItem = document.createElement("li");
-          listItem.textContent = `${book.title} by ${book.author} (${book.year})`;
-          listItem.addEventListener("click", function () {
-            displayBookDetails(index);
-            });
-        bookList.appendChild(listItem);
-        });
-    });
+  const year = document.createElement("p");
+  year.textContent = `Year: ${book.year}`;
 
-     // Get the sorting buttons
+  card.appendChild(image);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(year);
+
+  card.addEventListener("click", function () {
+    displayBookDetails(book);
+  });
+
+  return card;
+}
+
+  // Function to display book details
+  function displayBookDetails(book) {
+    bookDetails.title.textContent = book.title;
+    bookDetails.author.textContent = `Author: ${book.author}`;
+    bookDetails.year.textContent = `Year: ${book.year}`;
+    bookDetails.genre.textContent = `Genre: ${book.genre}`;
+    bookDetails.rating.textContent = `Rating: ${book.rating}`;
+    bookDetails.description.textContent = `Description: ${book.description}`;
+    bookDetails.image.src = book.image;
+    bookDetails.image.alt = book.title;
+  }
+
+  // Initial display of book cards
+  displayBookCards(books);
+
+  // Get the genre filter dropdown element
+  const genreFilter = document.getElementById("genre-filter");
+
+  // Add an event listener to the genre filter dropdown
+  genreFilter.addEventListener("change", function () {
+    const selectedGenre = genreFilter.value;
+    const filteredBooks = selectedGenre === "all" ? books : books.filter(book => book.genre === selectedGenre);
+    displayBookCards(filteredBooks);
+  });
+
+  // Get the sorting buttons
   const sortNewestButton = document.getElementById("sort-newest");
   const sortOldestButton = document.getElementById("sort-oldest");
 
@@ -76,28 +89,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to sort books by publication year
   function sortBooksByYear(newestFirst) {
-    // Create a copy of the books array to avoid modifying the original array
     const sortedBooks = [...books];
 
-    sortedBooks.sort(function (a, b) {
-      if (newestFirst) {
-        return b.year - a.year;
-      } else {
-        return a.year - b.year;
-      }
-    });
+    sortedBooks.sort((a, b) => (newestFirst ? b.year - a.year : a.year - b.year));
 
-    // Clear the existing book list
-    bookList.innerHTML = '';
-
-    // Loop through the sorted books and create list items
-    sortedBooks.forEach(function (book, index) {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${book.title} by ${book.author} (${book.year})`;
-      listItem.addEventListener("click", function () {
-        displayBookDetails(index);
-      });
-      bookList.appendChild(listItem);
-    });
+    displayBookCards(sortedBooks);
   }
 });
